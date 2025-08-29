82ee2ffab71cb72c37fe4c0ed593bb4204378781
Jaewon Choi <wefhio1985@gmail.com>
2025-08-29 15:24:33 +0900

    5주차 2번 풀이 제출

---
+++ week05/Jaewon/2166.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open("../../input/input.txt", 'r')
+input = sys.stdin.readline
+
+N = int(input())
+points = [tuple(map(int, input().split())) for _ in range(N)]
+
+area = 0
+for i in range(N):
+    x1, y1 = points[i]
+    x2, y2 = points[(i + 1) % N]
+    area += (x1 * y2) - (x2 * y1)
+
+print(round(abs(area) / 2, 1))
